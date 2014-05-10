0f9728944a2c968bbc04cf3ed5f53798b50530cc
renatus <pub@renat.biz>
2014-05-10 04:14:28 +0400

    Add UUIDs as a keys for entries

--- js/controllers.js
+++ js/controllers.js
@@ -296,8 +296,9 @@ function generateUUID4(){
 
 
 function generateUUID5(){
-'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
 });
+    return uuid;
 }

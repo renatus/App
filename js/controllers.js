305ab4396c458954dfb00effdb27784715cce4ce
renatus <pub@renat.biz>
2014-05-17 21:30:24 +0400

    Finalize IndexedDB support

--- js/controllers.js
+++ js/controllers.js
@@ -40,7 +40,7 @@ phonecatApp.controller('StartCtrl', function ($scope, indexedDBexo) {
         
         var newEntry = {
             "uuid": UUID4,
-			"title": ["en": $scope.activity.title],
+			"title": {"en": $scope.activity.title},
             "language": "English",
             "langcode": "en",
 			"createdTimeStamp": curTimestamp,

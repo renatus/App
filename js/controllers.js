e145e874be6d62fe8119c25ffa783e2aca0362ab
renatus <pub@renat.biz>
2014-05-17 22:56:54 +0400

    Finalize IndexedDB support

--- js/controllers.js
+++ js/controllers.js
@@ -40,7 +40,7 @@ app.controller('StartCtrl', function ($scope, indexedDBexo) {
         
         var newEntry = {
             "uuid": UUID4,
-			"title": {"en": $scope.activity.title},
+			"title": {"en": $scope.activity.title, "ru": "Русский заголовок"},
             "language": "English",
             "langcode": "en",
 			"createdTimeStamp": curTimestamp,

210fdb2f436373689ea8f30b99111e2c17c7fccd
renatus <pub@renat.biz>
2014-05-23 02:21:42 +0400

    Finalize IndexedDB support

--- js/controllers.js
+++ js/controllers.js
@@ -17,6 +17,22 @@ app.controller('StartCtrl', function ($scope, indexedDBexo) {
         
         return true; // otherwise it won't be within the results
     };
+    
+    $scope.filter('orderObjectBy', function() {
+        return function(items, field, reverse) {
+            var filtered = [];
+            angular.forEach(items, function(item) {
+                filtered.push(item);
+            });
+            
+            filtered.sort(function (a, b) {
+                return (a[field] > b[field] ? 1 : -1);
+            });
+            if(reverse) filtered.reverse();
+            return filtered;
+        };
+    });
+
 	
 	
 	

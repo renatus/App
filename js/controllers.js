c153fed03dca849ab5b97326d7b4fd2667e8efaa
renatus <pub@renat.biz>
2014-05-24 05:50:20 +0400

    Finalize IndexedDB support

--- js/controllers.js
+++ js/controllers.js
@@ -10,7 +10,7 @@ var app = angular.module('testApp', ['exoFilters']);
             }
             
             array.sort(function(a, b){
-                console.log(b[attribute]);
+                console.log(b);
                 
                 a = parseInt(a[attribute]);
                 b = parseInt(b[attribute]);

e9210eb38f8486145c45551b8f33e81e007d67d9
renatus <pub@renat.biz>
2014-05-26 18:08:36 +0400

    Finalize IndexedDB support

--- js/controllers.js
+++ js/controllers.js
@@ -9,7 +9,7 @@ app.filter('orderObjectBy', function(){
         if (!angular.isObject(input)) return input;
             
         var array = [];
-        for(var objectKey in input) {
+        for (var objectKey in input) {
             array.push(input[objectKey]);
         }
         

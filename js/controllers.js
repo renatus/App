d93d8c2a6899c9d3a3445822c71f3ffd08362259
renatus <pub@renat.biz>
2014-07-06 19:16:06 +0400

    Fix revisions

--- js/controllers.js
+++ js/controllers.js
@@ -442,7 +442,7 @@ app.directive("editActivity", function() {
             $scope.editbActivityLangcode = angular.copy($scope['activity'][$scope['activity']['lastVersion']]['langcode']);
             $scope.editbActivityLastRev = angular.copy($scope['activity'][$scope['activity']['lastVersion']]);
             
-            console.log($scope['activity'][$scope['activity']['lastVersion']]);
+            console.log("This obj: " + $scope['activity'][$scope['activity']['lastVersion']]);
             //$scope.editbActivity = JSON.parse(JSON.stringify($scope.activity));
             
             //Will delete only reference, not object itself, but that may help to garbage collector 
